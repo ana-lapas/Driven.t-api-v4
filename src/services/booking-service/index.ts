6cbeb5bd2e9c1dc77cbc7125e373df70bc473d15
ana-lapas <anapaulalapas@gmail.com>
2023-05-02 08:46:18 -0400

    Fix: post booking

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -43,7 +43,7 @@ async function postBookings(userId: number, roomId: number) {
   const checkBookings = await bookingRepository.getBookingsRoom(bookings.roomId);
   if (existingRoom.capacity <= checkBookings.length) throw forbiddenError();
 
-  return await bookingRepository.createBookings({ roomId, userId });
+  return bookingRepository.createBookings({ roomId, userId });
 }
 
 async function updateBookings(userId: number, bookingId: number, roomId: number) {
